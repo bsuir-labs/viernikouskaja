e61f4dfbcd113a9267d5fd5e7d4215fe39c8ba27
Kastus <bsuir-labs@yandex.by>
2020-06-07 06:22:28 +0300

    Fix indent

--- Laba 2 v0.5/Laba 2 v0.5/Source.cpp
+++ Laba 2 v0.5/Laba 2 v0.5/Source.cpp
@@ -26,96 +26,96 @@ void Sort(int left, int right);
 
 int main()
 {
-	for (int u = 0; u < MAXN; u++)
-		kol[u] = 0;
-	read();
-	find_bridges();
-	Sort(1, pol);
-	ofstream out("bridges.out");
-	out << pol << endl;
-	for (int y = 1; y <= pol; y++)
-		out << result[y] << " ";
-	out << endl << endl;
-	out.close();
-	return 0;
+    for (int u = 0; u < MAXN; u++)
+        kol[u] = 0;
+    read();
+    find_bridges();
+    Sort(1, pol);
+    ofstream out("bridges.out");
+    out << pol << endl;
+    for (int y = 1; y <= pol; y++)
+        out << result[y] << " ";
+    out << endl << endl;
+    out.close();
+    return 0;
 }
 
 void read()
 {
-	int Dorogi;
-	int t1, t2;
-	ifstream fin("bridges.in");
-	fin >> n;
-	fin >> Dorogi;
-	for (int i = 0; i < Dorogi; i++)
-	{
-		if (fin.eof())
-			break;
-		fin >> t1;
-		fin >> t2;
-		--t1;
-		--t2;
-		g[t1].push_back(t2);	//������ ���������
-		g[t2].push_back(t1);
-		num[t1][kol[t1]] = i + 1; kol[t1]++;
-		num[t2][kol[t2]] = i + 1; kol[t2]++;
-	}
-	fin.close();
+    int Dorogi;
+    int t1, t2;
+    ifstream fin("bridges.in");
+    fin >> n;
+    fin >> Dorogi;
+    for (int i = 0; i < Dorogi; i++)
+    {
+        if (fin.eof())
+            break;
+        fin >> t1;
+        fin >> t2;
+        --t1;
+        --t2;
+        g[t1].push_back(t2);    //������ ���������
+        g[t2].push_back(t1);
+        num[t1][kol[t1]] = i + 1; kol[t1]++;
+        num[t2][kol[t2]] = i + 1; kol[t2]++;
+    }
+    fin.close();
 }
 
 void find_bridges()
 {
-	timer = 0;
-	for (int i = 0; i < n; ++i)
-		used[i] = false;
-	for (int i = 0; i < n; ++i)
-		if (!used[i])
-			dfs(i,-1); //� �������
+    timer = 0;
+    for (int i = 0; i < n; ++i)
+        used[i] = false;
+    for (int i = 0; i < n; ++i)
+        if (!used[i])
+            dfs(i,-1); //� �������
 }
 
 //v - ������� �� ������� ���������� �����; p - ������� � ������� ������� �����.
 void dfs(int v, int p = -1)
 {
-	used[v] = true;
-	arrB[v] = arrA[v] = timer++;
-	for (size_t i = 0; i < g[v].size(); ++i)
-	{
-		int to = g[v][i];	//to ��� ������ ������� � ������� ������� V
+    used[v] = true;
+    arrB[v] = arrA[v] = timer++;
+    for (size_t i = 0; i < g[v].size(); ++i)
+    {
+        int to = g[v][i];    //to ��� ������ ������� � ������� ������� V
 
-		if (to == p)  continue; // ���� to - ������� � ������� ������� �����
-		if (used[to])
-			arrA[v] = min(arrA[v], arrB[to]);
-		else
-		{
-			dfs(to, v);
-			arrA[v] = min(arrA[v], arrA[to]);
-			if (arrA[to] > arrB[v])
-				res(v, to, i);
-		}
-	}
+        if (to == p)  continue; // ���� to - ������� � ������� ������� �����
+        if (used[to])
+            arrA[v] = min(arrA[v], arrB[to]);
+        else
+        {
+            dfs(to, v);
+            arrA[v] = min(arrA[v], arrA[to]);
+            if (arrA[to] > arrB[v])
+                res(v, to, i);
+        }
+    }
 }
 
 void res(int a, int b, int i)
 {
-	pol++;
-	result[pol] = num[a][i];
+    pol++;
+    result[pol] = num[a][i];
 }
 
 void Sort(int left, int right)
 {
-	int i = left, j = right;
-	int t, x;
-	x = result[(i + j) / 2];
-	do {
-		while (result[i] < x && i < right) i++;
-		while (result[j] > x && j > left) j--;
-		if (i <= j) {
-			t = result[i];
-			result[i] = result[j];
-			result[j] = t;
-			i++; j--;
-		}
-	} while (i <= j);
-	if (left < j) Sort(left, j);
-	if (i < right) Sort(i, right);
+    int i = left, j = right;
+    int t, x;
+    x = result[(i + j) / 2];
+    do {
+        while (result[i] < x && i < right) i++;
+        while (result[j] > x && j > left) j--;
+        if (i <= j) {
+            t = result[i];
+            result[i] = result[j];
+            result[j] = t;
+            i++; j--;
+        }
+    } while (i <= j);
+    if (left < j) Sort(left, j);
+    if (i < right) Sort(i, right);
 }
